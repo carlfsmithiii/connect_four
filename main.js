ff61dc401d20ec8ccf30fca3669b2f33d2954c9d
Carl Smith <carlfsmithiii@gmail.com>
2018-08-20 14:06:24 -0400

    checks for vertical and horizontal victory

--- main.js
+++ main.js
@@ -21,11 +21,12 @@ function initGameArray() {
 gameGridNode.addEventListener("click", function dropToken(event) {
     const token = getNewGameToken();
     const columnNode = event.target;
-    const columnNumber = columnNode.dataset.column;
+    const columnNumber = Number(columnNode.dataset.column);
     const tokenColor = token.dataset.color;
     gameGridArray[columnNumber].push([tokenColor]);
     columnNode.appendChild(token);
     checkVerticalWin(columnNumber, tokenColor);
+    checkHorizontalWin(columnNumber, columnNode.childElementCount - 1, tokenColor);
 });
 
 function getNewGameToken() {
@@ -53,6 +54,50 @@ function toggleCurrentColor() {
 function checkVerticalWin(columnNumber, colorString) {
     if (gameGridArray[columnNumber].join('').includes(colorString.repeat(4))) {
         console.log("WIN");
+    }
+}
+
+function checkHorizontalWin(columnNumber, rowNumber, colorString) {
+    if ((checkLeft(columnNumber, rowNumber, colorString) + checkRight(columnNumber, rowNumber, colorString) - 1) >= 4) {
+        console.log("WIN");
+    } 
+}
+
+function checkLeft(columnNumber, rowNumber, colorString) {
+    if (!isFilledArrayPosition(columnNumber, rowNumber) || gameGridArray[columnNumber][rowNumber] != colorString) {
+        return 0;
+    } else {
+        // console.log(gameGridArray[columnNumber][rowNumber]);
+        return 1 + checkLeft(columnNumber - 1, rowNumber, colorString);
+    }
+}
+
+function checkRight(columnNumber, rowNumber, colorString) {
+    if (!isFilledArrayPosition(columnNumber, rowNumber) || gameGridArray[columnNumber][rowNumber] != colorString) {
+        return 0;
+    } else {
+        return 1 + checkRight(columnNumber + 1, rowNumber, colorString);
+    }
+}
+
+function checkUpRight(columnNumber, rowNumber, colorString) {
+    if (!isFilledArrayPosition(columnNumber, rowNumber) || gameGridArray[columnNumber][rowNumber] != colorString) {
+        return 0;
+    } else {
+        return 1 + checkRight(Number(columnNumber) + 1, rowNumber, colorString);
+    }
+}
+
+function isFilledArrayPosition(columnNumber, rowNumber) {
+    try {
+        let accessed = gameGridArray[columnNumber][rowNumber];
+        return true;
+    } catch (error) {
+        if (error instanceof TypeError) {
+            return false;
+        } else {
+            throw error;
+        }
     }
 }
 
